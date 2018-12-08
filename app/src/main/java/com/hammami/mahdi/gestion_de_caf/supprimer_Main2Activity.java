d8df9e305a8f2c4b4b6c8f10c5dc90bcd0c53205
acer <hammamimahdi708@gmail.com>
2018-12-08 22:54:41 +0100

    my first project

---
+++ app/src/main/java/com/hammami/mahdi/gestion_de_caf/supprimer_Main2Activity.java
@@ -0,0 +1,13 @@
+package com.hammami.mahdi.gestion_de_caf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class supprimer_Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supprimer__main2);
+    }
+}
