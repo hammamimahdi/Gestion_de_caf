07edaf2c9791641e3498ef7f2b1ef7c7100c47bf
acer <hammamimahdi708@gmail.com>
2018-12-08 22:32:04 +0100

    my first project

---
+++ app/src/main/java/com/hammami/mahdi/gestion_de_caf/ajout_Main2Activity.java
@@ -0,0 +1,13 @@
+package com.hammami.mahdi.gestion_de_caf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ajout_Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout__main2);
+    }
+}
