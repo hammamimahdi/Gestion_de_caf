165ebec4ff4d6851ca062b4775f5753ce057cba1
acer <hammamimahdi708@gmail.com>
2018-12-08 22:54:19 +0100

    my first project

--- app/src/main/java/com/hammami/mahdi/gestion_de_caf/accueil_adminMain2Activity.java
+++ app/src/main/java/com/hammami/mahdi/gestion_de_caf/accueil_adminMain2Activity.java
@@ -1,7 +1,9 @@
 package com.hammami.mahdi.gestion_de_caf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class accueil_adminMain2Activity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class accueil_adminMain2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_admin_main2);
+    }
+
+    public void bu_ajouter(View view) {
+        Intent intent = new Intent(this, ajout_Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void bu_modifier(View view) {
+        Intent intent = new Intent(this, modifier_Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void by_supprimer(View view) {
+        Intent intent = new Intent(this, supprimer_Main2Activity.class);
+        startActivity(intent);
     }
 }
